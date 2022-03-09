efae885124370aa7e8af0ce6fb31cc45ea7264d3
kapepa <kapepayx@gmail.com>
2022-03-10 01:31:19 +0200

    10.03

--- client/page/group/index.tsx
+++ client/page/group/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useContext, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, SectionList, SafeAreaView} from 'react-native';
 import GroupItems from '../../components/group.items/index';
 import ButtonPlus from '../../components/button.plus';
 import { GroopItemDto, GroopDto } from '../../dto/group.dto.tsx';
+import { UserAllContext } from '../../App.tsx';
 import StylesCSS from './styles.tsx';
 
 import { Ionicons } from '@expo/vector-icons';
@@ -15,22 +16,30 @@ interface IState {
   activeProfile: GroopItemDto,
 }
 
-export default function GroupPage<IGroupPage> ( { navigation, route } ) {
-  const { data } = route.params;;
-  const [state, setState] = useState(data);
+export default function GroupPage<IGroupPage> ( { navigation } ) {
+  const userAll = useContext(UserAllContext);
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    if(userAll.length)setState(userAll)
+  },[userAll])
 
   return (
-    <SafeAreaView style={styles.container}>
-      <SectionList
-        style={styles.group}
-        sections={data}
-        keyExtractor={(item, index) => item.phone + index }
-        renderItem={({ item }) => { return <View style={styles.groupInner}><GroupItems { ...{...item, navigation} } /></View> }}
-        renderSectionHeader={({ section: { date } }) => {
-            return (<Text style={styles.groupTitle}>{ date }</Text>)
-        }}
-      />
-    </SafeAreaView>
+    <>
+      {state.length ? (
+        <SafeAreaView style={styles.container}>
+          <SectionList
+            style={styles.group}
+            sections={state}
+            keyExtractor={(item, index) => item.phone + index }
+            renderItem={({ item }) => { return <View style={styles.groupInner}><GroupItems { ...{...item, navigation} } /></View> }}
+            renderSectionHeader={({ section: { date } }) => {
+                return (<Text style={styles.groupTitle}>{ date }</Text>)
+            }}
+          />
+        </SafeAreaView>
+      ) : null}
+    </>
   )
 }
 
